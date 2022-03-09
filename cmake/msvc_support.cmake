b822d865192efebf0428dbd46d58a8910c5d181b
xuejun <Xuejun.Zhai@intel.com>
2022-03-09 13:43:57 +0800

    [remove compiler-option] RPCLIB_MSVC_STATIC_RUNTIME
    Signed-off-by: xuejun <Xuejun.Zhai@intel.com>

--- cmake/msvc_support.cmake
+++ cmake/msvc_support.cmake
@@ -1,11 +1,6 @@
 
 function(rpclib_msvc_support)
   if(MSVC)
-    # When building via conan, respect the compilation settings.
-    if ("${CONAN_LINK_RUNTIME}" STREQUAL "/MT")
-      set(RPCLIB_MSVC_STATIC_RUNTIME ON)
-    endif()
-
     target_compile_definitions(${PROJECT_NAME} PRIVATE
       "WIN32_LEAN_AND_MEAN"
       "NOMINMAX"
@@ -19,33 +14,5 @@ function(rpclib_msvc_support)
       "ASIO_HAS_CSTDINT"
       "ASIO_HAS_STD_SHARED_PTR"
       "ASIO_HAS_STD_TYPE_TRAITS")
-
-    # MSVC static runtime support
-    #
-    # While this pollutes global flags (when using add_library), you would not want to
-    # build with a disparity anyway. (also, CMake still has no support for this, so you
-    # would end up doing something like this yourself).
-    if (RPCLIB_MSVC_STATIC_RUNTIME)
-      set(variables
-        CMAKE_C_FLAGS_DEBUG
-        CMAKE_C_FLAGS_MINSIZEREL
-        CMAKE_C_FLAGS_RELEASE
-        CMAKE_C_FLAGS_RELWITHDEBINFO
-        CMAKE_CXX_FLAGS_DEBUG
-        CMAKE_CXX_FLAGS_MINSIZEREL
-        CMAKE_CXX_FLAGS_RELEASE
-        CMAKE_CXX_FLAGS_RELWITHDEBINFO
-        )
-      message(STATUS
-        "MSVC -> forcing use of statically-linked runtime."
-        )
-
-      foreach(variable ${variables})
-        if(${variable} MATCHES "/MD")
-          string(REGEX REPLACE "/MD" "/MT" ${variable} "${${variable}}")
-        endif()
-      endforeach()
-
-    endif()
   endif()
 endfunction()
